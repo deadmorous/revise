58ef74e2531ef703a329b9d03814d7b8e2675b8e
Stepan Orlov <majorsteve@mail.ru>
2021-06-23 02:09:10 +0300

    Finished CGNS writer (program named to_cgns). Writes first timestep only.

--- examples/to_cgns/main.cpp
+++ examples/to_cgns/main.cpp
@@ -63,6 +63,68 @@ ElementType_t cgElementType(MeshElementType t) noexcept
     }
 }
 
+void writeZoneNodes(int out_file, int out_base, int out_zone, const MeshDataProvider::Zone& zone, size_t dim)
+{
+    constexpr auto dataType = cgDataType<real_type>();
+    auto nodeCount = zone.nodeCount();
+    auto nodeRange = zone.nodes();
+    for (size_t d=0; d<dim; ++d)
+    {
+        std::vector<real_type> coord(nodeCount);
+        boost::range::transform(nodeRange, coord.begin(), [d](auto& nodeCoords) {return nodeCoords[d]; });
+        auto coordName = "Coordinate" + std::string( 1, 'X' + d );
+        CGCALL(cg_coord_write, out_file, out_base, out_zone, dataType, coordName.c_str(), coord.data());
+    }
+}
+
+void writeZoneElements(int out_file, int out_base, int out_zone, const MeshDataProvider::Zone& zone)
+{
+    auto elementCount = zone.elementCount();
+    auto elementType = cgElementType(zone.elementType());
+    auto elementRange = zone.elements();
+    auto elementNodeCount = elementRange.front().size();
+    std::vector<cgsize_t> connectivity(elementCount*elementNodeCount);
+    auto ptr = connectivity.data();
+    for (auto e : elementRange) {
+        BOOST_ASSERT(e.size() == elementNodeCount);
+        boost::range::copy(e, ptr);
+        ptr += elementNodeCount;
+    }
+    // Indices in connectivity start with one!
+    boost::range::transform(
+        connectivity, connectivity.begin(),
+        [](auto x) { return x + 1; });
+    CGCALL(cg_section_write,
+           out_file, out_base, out_zone,
+           "Elem", elementType, 1, elementCount, 0, connectivity.data());
+
+}
+
+void writeZoneFields(
+    int out_file, int out_base, int out_zone,
+    const MeshDataProvider::Zone& zone, const std::vector<unsigned int>& fieldVar,
+    const std::vector<std::string>& variableNames)
+{
+    constexpr auto dataType = cgDataType<real_type>();
+    auto out_sol = CGCALL(cg_sol_write, out_file, out_base, out_zone, "Fields", Vertex);
+
+    auto nodeCount = zone.nodeCount();
+    auto nodeRange = zone.nodes();
+
+    for (size_t ifield=0, nfields=fieldVar.size(); ifield<nfields; ++ifield )
+    {
+        std::vector<real_type> field(nodeCount);
+        boost::range::transform(
+            nodeRange, field.begin(),
+            [ifield](auto& nodeCoords)
+            { return nodeCoords[ifield]; });
+        auto fieldName = variableNames.at(fieldVar[ifield]);
+        CGCALL(
+            cg_field_write, out_file, out_base, out_zone, out_sol,
+            dataType, fieldName.c_str(), field.data());
+    }
+}
+
 void run(int argc, char* argv[])
 {
     std::string inputName;
@@ -95,71 +157,55 @@ void run(int argc, char* argv[])
 
     auto [mainMeshFileName, hasTimeSteps] = firstOutputFrameFileName(inputName);
     REPORT_PROGRESS_STAGES();
-    REPORT_PROGRESS_STAGE("TODO: Stage 1");
+    REPORT_PROGRESS_STAGE("Open input and output files");
     MeshDataProvider mainMeshProvider(mainMeshFileName);
     cout << "Opened input file " << mainMeshFileName << endl
          << "Variables: "
          << boost::join(mainMeshProvider.variables(), ", ") << endl;
 
-    auto coordVar = mainMeshProvider.coordinateVariables();
-    auto dim = coordVar.size();
-
+    // Open CGNS output file; create base node
     auto out_file = cgcall(
         cg_open,
         [&]{ return "Failed to open output file '" + outputName + "'"; },
         outputName.c_str(), CG_MODE_WRITE);
-
+    auto coordVar = mainMeshProvider.coordinateVariables();
+    auto dim = coordVar.size();
     auto out_base = CGCALL(cg_base_write, out_file, "base", dim, dim);
 
-    constexpr auto dataType = cgDataType<real_type>();
-
+    // Write mesh (nodes and elements)
+    REPORT_PROGRESS_STAGE("Write Mesh");
     auto cache = mainMeshProvider.makeCache();
-    auto zoneNumber = 0;
+    std::vector<int> zoneIds;
     for (auto zone : mainMeshProvider.zones(cache, coordVar))
     {
-        auto zoneName = string("zone_") + to_string(zoneNumber++);
-
+        auto zoneName = string("zone_") + to_string(zoneIds.size());
+        REPORT_PROGRESS_STAGE(zoneName);
         auto nodeCount = zone.nodeCount();
-        auto nodeRange = zone.nodes();
         BOOST_ASSERT(nodeCount > 0);
-
         auto elementCount = zone.elementCount();
         cgsize_t size[] = {
-            nodeCount,
-            elementCount,
+            static_cast<cgsize_t>(nodeCount),
+            static_cast<cgsize_t>(elementCount),
             0
         };
         auto out_zone = CGCALL(cg_zone_write, out_file, out_base, zoneName.c_str(), size, Unstructured);
-
-        for (size_t d=0; d<dim; ++d)
-        {
-            std::vector<real_type> coord(nodeCount);
-            boost::range::transform(nodeRange, coord.begin(), [d](auto& nodeCoords) {return nodeCoords[d]; });
-            auto coordName = "Coordinate" + std::string( 1, 'X' + d );
-            CGCALL(cg_coord_write, out_file, out_base, out_zone, dataType, coordName.c_str(), coord.data());
-        }
-
-        auto elementType = cgElementType(zone.elementType());
-        std::vector<size_t> indices;
-        BOOST_ASSERT(elementCount > 0);
-        auto elementRange = zone.elements();
-        auto elementNodeCount = elementRange.front().size();
-        // std::vector<cgsize_t> connectivity(elementCount*elementNodeCount);
-        std::vector<uint32_t> connectivity(elementCount*elementNodeCount);
-        auto ptr = connectivity.data();
-        for (auto e : elementRange) {
-            BOOST_ASSERT(e.size() == elementNodeCount);
-            boost::range::copy(e, ptr);
-            ptr += elementNodeCount;
-        }
-        // Indices in connectivity start with one!
-        boost::range::transform(
-            connectivity, connectivity.begin(),
-            [](auto x) { return x + 1; });
-        CGCALL(cg_section_write,
-            out_file, out_base, out_zone,
-            "Elem", elementType, 1, elementCount, 0, reinterpret_cast<const cgsize_t*>(connectivity.data()));
+        zoneIds.push_back(out_zone);
+        writeZoneNodes(out_file, out_base, out_zone, zone, dim);
+        writeZoneElements(out_file, out_base, out_zone, zone);
     }
+
+    REPORT_PROGRESS_STAGE("Write fields");
+    auto fieldVar = mainMeshProvider.fieldVariables();
+    auto izone = 0;
+    auto fieldNames = mainMeshProvider.variables();
+    for (auto zone : mainMeshProvider.zones(cache, fieldVar))
+    {
+        auto zoneName = string("zone_") + to_string(zoneIds.size());
+        REPORT_PROGRESS_STAGE(zoneName);
+        writeZoneFields(out_file, out_base, zoneIds.at(izone), zone, fieldVar, fieldNames);
+        ++izone;
+    }
+
     if (cg_close(out_file) != CG_OK)
         throw runtime_error("Failed to close file '" + mainMeshFileName + "'");
 }
